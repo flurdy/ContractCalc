b5271faca71ad9f7bf970814eeead38df40eae34
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-01-23 22:53:20 +0000

    beta!

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,11 +16,21 @@ import models.*;
 
 public class Application extends Controller {
 
+    private static final int WORKDAYS_PER_YEAR = 260;
+
     public static void index() {
         render();
     }
-    
-    public static void calculate(
+
+    public static void rateRedirect() {
+        enterRate();
+    }
+
+    public static void enterRate(){
+         render("Application/calculateRate.html");
+    }
+
+    public static void calculateRate(
             @Required(message = "Salary required") @Min(1) BigDecimal salary,
             @Required(message = "Cost required")
             @Min(1) @Max(5) BigDecimal cost,
@@ -35,21 +45,84 @@ public class Application extends Controller {
     ){
        if(validation.hasErrors()) {
            params.flash();
-           validation.keep(); 
-           index();
+           validation.keep();
+           enterRate();
        }
-        BigDecimal calculation = salary.multiply(cost)
+        BigDecimal calculation = salary.multiply(cost).multiply(tax)
                 .divide(hoursYear, RoundingMode.HALF_UP)
-                .divide(utilisation, RoundingMode.HALF_UP)
-                .multiply(tax);
+                .divide(utilisation, RoundingMode.HALF_UP);
         final NumberFormat numberFormat = NumberFormat.getInstance(Locale.UK);
         String hourlyRate = numberFormat.format(Math.round(calculation.doubleValue()));
         BigDecimal dailyCalculation = new BigDecimal(hourlyRate).multiply(hoursDay);
         String dailyRate = numberFormat.format(Math.round(dailyCalculation.doubleValue()));
-        BigDecimal yearCalculation = new BigDecimal(hourlyRate).multiply(hoursYear);
+        BigDecimal yearCalculation = new BigDecimal(hourlyRate).multiply(hoursYear)
+                .divide(utilisation, RoundingMode.HALF_UP);
         String yearIncome = numberFormat.format(Math.round(yearCalculation.doubleValue()));
         params.flash();
-        render("Application/index.html",hourlyRate,dailyRate,yearIncome);
+        render(hourlyRate,dailyRate,yearIncome);
+    }
+
+    public static void hoursPerYear(){
+        render();
+    }
+
+    public static void hoursPerYearRedirect(){
+        hoursPerYear();
+    }
+
+    public static void calculateHoursPerYear(
+            @Required(message = "Daily hours required")
+            @Min(1) @Max(24) Integer hoursPerDay,
+            @Required(message = "Holidays required")
+            @Min(0) @Max(260) Integer holidays,
+            @Required(message = "Bank Holidays required")
+            @Min(0) @Max(260) Integer bankHolidays,
+            @Min(0) @Max(260) Integer trainingDays,
+            @Min(0) @Max(260) Integer adminDays,
+            @Required(message = "Sick days required")
+            @Min(0) @Max(260) Integer sickDays,
+            @Min(0) @Max(260) Integer benchDays ){
+        
+        if(validation.hasErrors()) {
+            params.flash();
+            validation.keep();
+            hoursPerYear();
+        }
+
+        final int optimisticDaysPerYear = WORKDAYS_PER_YEAR - bankHolidays;
+        final int optimisticHoursPerYear = optimisticDaysPerYear * hoursPerDay;
+        final int optimisticWeeksPerYear = optimisticDaysPerYear / 5;
+        final BigDecimal optimisticPercentOfYear = calculateYearlyPercentage(optimisticDaysPerYear);
+
+        int realisticDaysPerYear = optimisticDaysPerYear -holidays;
+        if( trainingDays != null ){  realisticDaysPerYear -= trainingDays; }
+        if( adminDays != null ){  realisticDaysPerYear -= adminDays; }
+//        if( sickDays != null ){  realisticDaysPerYear -= sickDays; }
+
+        final int realisticHoursPerYear = realisticDaysPerYear * hoursPerDay;
+        final int realisticWeeksPerYear = realisticDaysPerYear / 5;
+        final BigDecimal realisticPercentOfYear =  calculateYearlyPercentage(realisticDaysPerYear);
+
+        int pessimisticDaysPerYear = realisticDaysPerYear;
+        if( sickDays != null ){  pessimisticDaysPerYear -= sickDays; }
+        if( benchDays != null ){  pessimisticDaysPerYear -= benchDays; }
+
+        final int pessimisticHoursPerYear = pessimisticDaysPerYear * hoursPerDay;
+        final int pessimisticWeeksPerYear = pessimisticDaysPerYear / 5;
+        final BigDecimal pessimisticPercentOfYear =  calculateYearlyPercentage(pessimisticDaysPerYear);
+
+        params.flash();
+        render("Application/hoursPerYear.html",
+                optimisticWeeksPerYear,optimisticDaysPerYear,optimisticHoursPerYear,
+                realisticWeeksPerYear,realisticDaysPerYear,realisticHoursPerYear,
+                pessimisticWeeksPerYear,pessimisticDaysPerYear,pessimisticHoursPerYear,
+                optimisticPercentOfYear,realisticPercentOfYear,pessimisticPercentOfYear);
+    }
+
+    private static BigDecimal calculateYearlyPercentage(int daysPerYear){
+        return new BigDecimal(daysPerYear)
+                .divide(new BigDecimal(WORKDAYS_PER_YEAR), 4, RoundingMode.HALF_UP)
+                .multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP);
     }
 
 }
