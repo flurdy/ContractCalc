045ee60edea0d679738a310aaa396c1b28e44086
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-01-26 00:25:58 +0000

    tidied up with pojo

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,8 +15,6 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-
-    private static final int WORKDAYS_PER_YEAR = 260;
     private final NumberFormat numberFormat = NumberFormat.getInstance(Locale.UK);
 
     public static void index() {
@@ -62,17 +60,7 @@ public class Application extends Controller {
 
 
     public static void calculateHoursPerYear(
-            @Required(message = "Daily hours required")
-            @Min(1) @Max(24) Integer hoursPerDay,
-            @Required(message = "Holidays required")
-            @Min(0) @Max(260) Integer holidays,
-            @Required(message = "Bank Holidays required")
-            @Min(0) @Max(260) Integer bankHolidays,
-            @Min(0) @Max(260) Integer trainingDays,
-            @Min(0) @Max(260) Integer adminDays,
-            @Required(message = "Sick days required")
-            @Min(0) @Max(260) Integer sickDays,
-            @Min(0) @Max(260) Integer benchDays ){
+            @Required @Valid HoursPerYearCalculation hoursPerYearCalculation ){
         
         if(validation.hasErrors()) {
             params.flash();
@@ -80,27 +68,21 @@ public class Application extends Controller {
             hoursPerYear();
         }
 
-        final int optimisticDaysPerYear = WORKDAYS_PER_YEAR - bankHolidays;
-        final int optimisticHoursPerYear = optimisticDaysPerYear * hoursPerDay;
-        final int optimisticWeeksPerYear = optimisticDaysPerYear / 5;
-        final BigDecimal optimisticPercentOfYear = calculateYearlyPercentage(optimisticDaysPerYear);
+        final int optimisticDaysPerYear = hoursPerYearCalculation.calculateWorkDaysWithoutBankHolidays();
+        final BigDecimal optimisticHoursPerYear = hoursPerYearCalculation.calculateWorkHoursWithoutBankHolidays();
+        final int optimisticWeeksPerYear = hoursPerYearCalculation.calculateWorkWeeksWithoutBankHolidays();
+        final BigDecimal optimisticPercentOfYear = hoursPerYearCalculation.calculateYearlyPercentageWithoutBankHolidays();
 
-        int realisticDaysPerYear = optimisticDaysPerYear -holidays;
-        if( trainingDays != null ){  realisticDaysPerYear -= trainingDays; }
-        if( adminDays != null ){  realisticDaysPerYear -= adminDays; }
-//        if( sickDays != null ){  realisticDaysPerYear -= sickDays; }
+        final int realisticDaysPerYear = hoursPerYearCalculation.calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin();
+        final BigDecimal realisticHoursPerYear = hoursPerYearCalculation.calculateWorkHoursWithoutBankHolidaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin();
+        final int realisticWeeksPerYear = hoursPerYearCalculation.calculateWorkWeeksWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin();
+        final BigDecimal realisticPercentOfYear =  hoursPerYearCalculation.calculateYearlyPercentageWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin();
 
-        final int realisticHoursPerYear = realisticDaysPerYear * hoursPerDay;
-        final int realisticWeeksPerYear = realisticDaysPerYear / 5;
-        final BigDecimal realisticPercentOfYear =  calculateYearlyPercentage(realisticDaysPerYear);
+        int pessimisticDaysPerYear = hoursPerYearCalculation.calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench();
 
-        int pessimisticDaysPerYear = realisticDaysPerYear;
-        if( sickDays != null ){  pessimisticDaysPerYear -= sickDays; }
-        if( benchDays != null ){  pessimisticDaysPerYear -= benchDays; }
-
-        final int pessimisticHoursPerYear = pessimisticDaysPerYear * hoursPerDay;
-        final int pessimisticWeeksPerYear = pessimisticDaysPerYear / 5;
-        final BigDecimal pessimisticPercentOfYear =  calculateYearlyPercentage(pessimisticDaysPerYear);
+        final BigDecimal pessimisticHoursPerYear = hoursPerYearCalculation.calculateWorkHoursWithoutBankHolidaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench();
+        final int pessimisticWeeksPerYear = hoursPerYearCalculation.calculateWorkWeeksWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNoSicWithBench();
+        final BigDecimal pessimisticPercentOfYear =  hoursPerYearCalculation.calculateYearlyPercentageWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNoSicWithBench();
 
         params.flash();
         render("Application/hoursPerYear.html",
@@ -110,10 +92,5 @@ public class Application extends Controller {
                 optimisticPercentOfYear,realisticPercentOfYear,pessimisticPercentOfYear);
     }
 
-    private static BigDecimal calculateYearlyPercentage(int daysPerYear){
-        return new BigDecimal(daysPerYear)
-                .divide(new BigDecimal(WORKDAYS_PER_YEAR), 4, RoundingMode.HALF_UP)
-                .multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP);
-    }
 
 }
---
+++ app/models/HoursPerYearCalculation.java
@@ -0,0 +1,121 @@
+package models;
+
+import play.data.validation.Max;
+import play.data.validation.Min;
+import play.data.validation.Required;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class HoursPerYearCalculation {
+
+    public static final int WORKDAYS_PER_YEAR = 260;
+
+    @Required(message = "Daily hours required")
+    @Min(1) @Max(24) 
+    public BigDecimal hoursPerDay;
+    
+    @Required(message = "Holidays required")
+    @Min(0) @Max(260)
+    public Integer holidays;
+
+    @Required(message = "Bank Holidays required")
+    @Min(0) @Max(260)
+    public Integer bankHolidays;
+
+    @Min(0) @Max(260)
+    public Integer trainingDays;
+
+    @Min(0) @Max(260)
+    public Integer adminDays;
+
+    @Required(message = "Sick days required")
+    @Min(0) @Max(260)
+    public Integer sickDays;
+
+    @Min(0) @Max(260)
+    public Integer benchDays;
+
+
+    /*
+    public HoursPerYearCalculation(BigDecimal hoursPerDay, Integer holidays,
+                                   Integer bankHolidays, Integer trainingDays,
+                                   Integer adminDays, Integer sickDays,
+                                   Integer benchDays) {
+        this.hoursPerDay = hoursPerDay;
+        this.holidays = holidays;
+        this.bankHolidays = bankHolidays;
+        this.trainingDays = trainingDays;
+        this.adminDays = adminDays;
+        this.sickDays = sickDays;
+        this.benchDays = benchDays;
+    }
+    */
+
+    public int calculateWorkDaysWithoutBankHolidays() {
+        return WORKDAYS_PER_YEAR - bankHolidays;
+    }
+
+    public int calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin() {
+        int workDays = calculateWorkDaysWithoutBankHolidays() - holidays;
+        if( trainingDays != null ){  workDays -= trainingDays; }
+        if( adminDays != null ){  workDays -= adminDays; }
+//        if( sickDays != null ){  realisticDaysPerYear -= sickDays; }
+        return workDays;
+    }
+
+    public int calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench() {
+        int workDays = calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin();
+        if( sickDays != null ){  workDays -= sickDays; }
+        if( benchDays != null ){  workDays -= benchDays; }
+        return workDays;
+    }
+
+    public BigDecimal calculateWorkHours(int workDays) {
+        return hoursPerDay.multiply(new BigDecimal(workDays));
+    }
+
+    public BigDecimal calculateWorkHoursWithoutBankHolidays() {
+        return calculateWorkHours(calculateWorkDaysWithoutBankHolidays()) ;
+    }
+
+    public BigDecimal calculateWorkHoursWithoutBankHolidaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin() {
+        return calculateWorkHours(calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin());
+    }
+
+    public BigDecimal calculateWorkHoursWithoutBankHolidaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench() {
+        return calculateWorkHours(calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench());
+    }
+
+    public int calculateWorkWeeksWithoutBankHolidays() {
+        return calculateWorkDaysWithoutBankHolidays() / 5;
+    }
+
+    public int calculateWorkWeeksWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin() {
+        return calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench() / 5;
+    }
+
+    public int calculateWorkWeeksWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNoSicWithBench() {
+        return calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench() / 5;
+    }
+
+    private BigDecimal calculateYearlyPercentage(int daysWorked){
+        return new BigDecimal(daysWorked)
+                .divide(new BigDecimal(WORKDAYS_PER_YEAR), 4, RoundingMode.HALF_UP)
+                .multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal calculateYearlyPercentageWithoutBankHolidays(){
+        return calculateYearlyPercentage(calculateWorkDaysWithoutBankHolidays());
+    }
+
+    public BigDecimal calculateYearlyPercentageWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin(){
+        return calculateYearlyPercentage(calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdmin());
+    }
+
+    public BigDecimal calculateYearlyPercentageWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNoSicWithBench(){
+        return calculateYearlyPercentage(calculateWorkDaysWithoutBankHolidaysNorHolidaysNorTrainingNorAdminNorSicWithBench());
+    }
+
+
+}
