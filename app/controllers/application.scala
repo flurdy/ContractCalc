e357135adc96731a18331a956820f037da2ee5ee
ivar <ivar@flurdy.com>
2014-06-08 17:29:35 +0100

    redirects

--- app/controllers/application.scala
+++ app/controllers/application.scala
@@ -26,6 +26,10 @@ object TimeController extends Controller {
     Ok(views.html.time.hoursPerYear())
   }
 
+  def redirectHoursPerYear = Action {
+    Redirect(routes.TimeController.hoursPerYear)
+  }
+
   def calculateHoursPerYear = Action {
     Ok(views.html.time.hoursPerYear())
   }
@@ -39,6 +43,10 @@ object RateController extends Controller {
     Ok(views.html.rate.calculateRate())
   }
 
+  def redirectEnterRate = Action {
+    Redirect(routes.RateController.enterRate)
+  }
+
   def calculateRate = Action {
     Ok(views.html.rate.calculateRate())
   }
@@ -47,12 +55,20 @@ object RateController extends Controller {
     Ok(views.html.rate.enterIncomeRate())
   }
 
+  def redirectEnterIncomeRate = Action {
+    Redirect(routes.RateController.enterIncomeRate)
+  }
+
   def calculateIncome = Action {
     Ok(views.html.rate.enterIncomeRate())
   }
 
   def rateDifference = Action {
     Ok(views.html.rate.rateDifference())
+  }
+
+  def redirectRateDifference = Action {
+    Redirect(routes.RateController.rateDifference)
   }
 
   def calculateRateDifference = Action {
