d530a5e4886eee0e0aca085c7198beb3af5b7146
Ivar Abrahamsen <ivar@flurdy.com>
2016-01-05 23:40:54 +0000

    Bumped to Play 2.4 and more scalafied

--- app/controllers/application.scala
+++ app/controllers/application.scala
@@ -2,8 +2,10 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
-object Application extends Controller {
+class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index())
@@ -20,7 +22,24 @@ object Application extends Controller {
 }
 
 
-object TimeController extends Controller {
+class TimeController extends Controller {
+
+  val timeForm = Form(
+  	  tuple(
+  	  	 "hoursPerDay"  -> bigDecimal,
+  	  	 "holidays"     -> number(min = 0, max = 366),
+  	  	 "bankHolidays" -> number(min = 0, max = 366),
+  	  	 "sickDays"     -> number(min = 0, max = 366),
+  	  	 "trainingDays" -> number(min = 0, max = 366),
+  	  	 "adminDays"    -> number(min = 0, max = 366),
+  	  	 "benchDays"    -> number(min = 0, max = 366)
+  	  ) verifying ("hours per day not valid", fields =>
+  	  		fields match {
+  	  			case (hoursPerDay, _, _, _, _, _, _) => {
+  	  				hoursPerDay < BigDecimal(24) && hoursPerDay > BigDecimal(0)   	  			}
+  	  		}
+  	  )
+  )
 
   def hoursPerYear = Action {
     Ok(views.html.time.hoursPerYear())
@@ -30,14 +49,24 @@ object TimeController extends Controller {
     Redirect(routes.TimeController.hoursPerYear)
   }
 
-  def calculateHoursPerYear = Action {
-    Ok(views.html.time.hoursPerYear())
+  def calculateHoursPerYear = Action { implicit request =>
+
+	 timeForm.bindFromRequest.fold(
+        errors => {
+            Logger.warn("Contact failed: " + errors)
+            BadRequest(views.html.time.hoursPerYear(Some(errors)))
+        },
+        times => {
+        		// todo
+    			Ok(views.html.time.hoursPerYear(Some(timeForm)))
+        }
+    )
   }
 
 }
 
 
-object RateController extends Controller {
+class RateController extends Controller {
 
   def enterRate = Action {
     Ok(views.html.rate.calculateRate())
--- build.sbt
+++ build.sbt
@@ -2,10 +2,17 @@ name := "contractcalc"
 
 version := "3.0-SNAPSHOT"
 
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.6"
+
 libraryDependencies ++= Seq(
   cache,
-  "org.webjars" %% "webjars-play" % "2.2.1-2",
-  "org.webjars" % "jquery" % "1.11.1"
+  "org.webjars" %% "webjars-play" % "2.4.0-1",
+//  "org.webjars" % "bootstrap" % "3.3.6",
+  "org.webjars" % "jquery" % "2.1.4"
 )
 
-play.Project.playScalaSettings
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+routesGenerator := InjectedRoutesGenerator
