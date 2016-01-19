c99997cebef465a4bc4690ec42c4eb99b2f6caaa
Ivar Abrahamsen <ivar@flurdy.com>
2016-01-19 01:53:12 +0000

    Show forms

--- app/controllers/application.scala
+++ app/controllers/application.scala
@@ -4,46 +4,52 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import views._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index())
-  }
+  def index     = Action ( Ok(html.index()) )
 
-  def resources = Action {
-    Ok(views.html.resources())
-  }
+  def resources = Action ( Ok(html.resources()) )
 
-  def contact = Action {
-    Ok(views.html.contact())
-  }
+  def contact   = Action ( Ok(html.contact()) )
 
 }
 
 
+case class TimeForm(hoursPerDay: BigDecimal, holidays: Int, bankHolidays: Int,
+                    sickDays: Int, trainingDays: Int, adminDays: Int, benchDays: Int)
+
+
 class TimeController extends Controller {
 
+
   val timeForm = Form(
-  	  tuple(
-  	  	 "hoursPerDay"  -> bigDecimal,
-  	  	 "holidays"     -> number(min = 0, max = 366),
-  	  	 "bankHolidays" -> number(min = 0, max = 366),
-  	  	 "sickDays"     -> number(min = 0, max = 366),
-  	  	 "trainingDays" -> number(min = 0, max = 366),
-  	  	 "adminDays"    -> number(min = 0, max = 366),
-  	  	 "benchDays"    -> number(min = 0, max = 366)
-  	  ) verifying ("hours per day not valid", fields =>
+  	  mapping(
+  	  	 "hoursPerDay"  -> default(bigDecimal(4,2), BigDecimal("8.0")),
+  	  	 "holidays"     -> default(number(min = 0, max = 366), 25),
+  	  	 "bankHolidays" -> default(number(min = 0, max = 366), 9),
+         "sickDays"     -> default(number(min = 0, max = 366), 4),
+  	  	 "trainingDays" -> default(number(min = 0, max = 366), 5),
+  	  	 "adminDays"    -> default(number(min = 0, max = 366), 5),
+  	  	 "benchDays"    -> default(number(min = 0, max = 366), 15)
+  	  )(TimeForm.apply)(TimeForm.unapply) verifying ("hours per day not valid", fields =>
   	  		fields match {
-  	  			case (hoursPerDay, _, _, _, _, _, _) => {
-  	  				hoursPerDay < BigDecimal(24) && hoursPerDay > BigDecimal(0)
-          	}
+  	  			case data => data.hoursPerDay < BigDecimal(24) && data.hoursPerDay > BigDecimal(0)
   	  		}
   	  )
   )
 
-  def hoursPerYear = Action {
-    Ok(views.html.time.hoursPerYear())
+  def hoursPerYear = Action {  implicit request =>
+    Ok(views.html.time.hoursPerYear(
+      timeForm.fill( TimeForm(
+            hoursPerDay  = BigDecimal("8").setScale(2),
+            holidays     = 5,
+            bankHolidays = 9,
+            sickDays     = 4,
+            trainingDays = 5,
+            adminDays    = 5,
+            benchDays    = 15 ))))
   }
 
   def redirectHoursPerYear = Action {
@@ -54,12 +60,12 @@ class TimeController extends Controller {
 
 	 timeForm.bindFromRequest.fold(
         errors => {
-            Logger.warn("Contact failed: " + errors)
-            BadRequest(views.html.time.hoursPerYear(Some(errors)))
+            Logger.warn("hoursPerYear failed: " + errors)
+            BadRequest(views.html.time.hoursPerYear(errors))
         },
         times => {
-        		// todo
-    			Ok(views.html.time.hoursPerYear(Some(timeForm)))
+          // todo
+    			Ok(views.html.time.hoursPerYear(timeForm.fill(times)))
         }
     )
   }
@@ -67,10 +73,67 @@ class TimeController extends Controller {
 }
 
 
+case class CalculateRateForm(
+  salary: Int, hoursPerYear: Int, cost: Int, utilisation: Int, tax: Int, risk: Int )
+
+
+trait RateLength {
+    val name: String
+}
+
+object RateLength {
+
+  def apply(rateLength: String): RateLength = {
+    rateLength.toLowerCase match {
+      case "day" => RatePerDay
+      case "hour" => RatePerHour
+      case _ => throw new IllegalArgumentException("Unknown rate length")
+    }
+  }
+
+  def unapply(rateLength: RateLength): Option[String] = Some(rateLength.name)
+
+}
+
+case object RatePerDay extends RateLength {
+  val name = "day"
+}
+
+case object RatePerHour extends RateLength{
+  val name = "hour"
+}
+
+case class CalculateIncomeForm( rate: Int, rateLength: RateLength, 
+                                chargeUnitsPerYear: Int, cost: Int, utilisation: Int)
+
+case class CalculateRateDifferenceForm(
+        hoursPerYear: Int, hoursPerDay: BigDecimal,
+        rateOne: Int, rateTwo: Int, rateLength: RateLength, benchTime: Int)
+
+
 class RateController extends Controller {
 
+  val calculateRateForm = Form(
+      mapping(
+          "salary"       -> default(number,50000),
+          "hoursPerYear" -> number(min=0, max=3000),
+          "cost"         -> number(min=0, max=100),
+          "utilisation"  -> number(min=0, max=110),
+          "tax"          -> number(min=50,max=100),
+          "risk"         -> number(min=0, max=100)
+      )(CalculateRateForm.apply)(CalculateRateForm.unapply)
+    )
+
   def showEnterIncomeToCalculateRate = Action {
-    Ok(views.html.rate.calculateRate())
+    Ok(views.html.rate.calculateRate(
+       calculateRateForm.fill( CalculateRateForm(
+        salary       = 50000,
+        hoursPerYear = 1600,
+        cost         = 30,
+        utilisation  = 90,
+        tax          = -10,
+        risk         = 0
+      ) ) ) )
   }
 
   def redirectEnterIncomeToCalculateRate = Action {
@@ -78,11 +141,31 @@ class RateController extends Controller {
   }
 
   def calculateRateFromIncome = Action {
+    // todo
     Redirect(routes.RateController.showEnterIncomeToCalculateRate)
   }
 
+  val calculateIncomeForm = Form(
+      mapping(
+          "rate"         -> default(number, 400),
+          "rate-length"   -> mapping(
+            "rate-length" -> default(nonEmptyText,"day")
+          )(RateLength.apply)(RateLength.unapply),
+          "charge-units-per-year" -> number(min=1,max=3000),
+          "cost"         -> number(min=0,max=100),
+          "utilisation"  -> number(min=0,max=110)
+      )(CalculateIncomeForm.apply)(CalculateIncomeForm.unapply)
+    )
+
   def showEnterRateToCalculateIncome = Action {
-    Ok(views.html.rate.calculateIncome())
+    Ok(views.html.rate.calculateIncome(calculateIncomeForm.fill(
+        CalculateIncomeForm(
+          rate = 400,
+          rateLength = RatePerDay,
+          chargeUnitsPerYear = 210,
+          cost = 20,
+          utilisation = 90
+      ))))
   }
 
   def redirectEnterRateToCalculateIncome = Action {
@@ -90,11 +173,30 @@ class RateController extends Controller {
   }
 
   def calculateIncomeFromRate = Action {
+    // todo
     Redirect(routes.RateController.showEnterRateToCalculateIncome)
   }
 
+  val calculateRateDifferenceForm = Form(
+    mapping(
+      "hoursPerYear" -> default(number(min=0,max=3000), 1600),
+      "hoursPerDay"  ->  default(bigDecimal, BigDecimal("7.5").setScale(1)),
+      "rateOne"      -> default(number(min=0), 400),
+      "rateTwo"      -> default(number(min=0), 500),
+      "rate-length"  -> mapping(
+          "rate-length" -> default(nonEmptyText, "day")
+        )(RateLength.apply)(RateLength.unapply),
+      "benchTime"    -> default(number(min=0,max=365), 0)
+      )(CalculateRateDifferenceForm.apply)(CalculateRateDifferenceForm.unapply)
+    )
+
   def showRateDifference = Action {
-    Ok(views.html.rate.rateDifference())
+    Ok(views.html.rate.rateDifference(calculateRateDifferenceForm.fill(
+        CalculateRateDifferenceForm(
+        hoursPerYear = 1600, hoursPerDay = BigDecimal("8").setScale(1),
+        rateOne = 400, rateTwo = 500, rateLength = RatePerDay, benchTime = 0
+
+    ))))
   }
 
   def redirectRateDifference = Action {
@@ -102,6 +204,7 @@ class RateController extends Controller {
   }
 
   def calculateRateDifference = Action {
+    // todo
     Redirect(routes.RateController.showRateDifference)
   }
 
